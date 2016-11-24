1486608dd787db3cab4f42d4f8a5a7484e46c996
Simon Brouard <sbrouard@users.noreply.github.com>
2016-11-24 08:19:54 +0000

    ajout de donnees dans la base

--- base.sql
+++ base.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client: localhost
--- Généré le: Mar 22 Novembre 2016 à 17:57
+-- Généré le: Jeu 24 Novembre 2016 à 09:17
 -- Version du serveur: 5.5.29-0ubuntu0.12.04.2
 -- Version de PHP: 5.3.10-1ubuntu3.25
 
@@ -33,6 +33,16 @@ CREATE TABLE IF NOT EXISTS `Appartenir_catégorie` (
   KEY `id_recette` (`id_recette`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Contenu de la table `Appartenir_catégorie`
+--
+
+INSERT INTO `Appartenir_catégorie` (`nom_catégorie`, `id_recette`) VALUES
+('Déssert', 1),
+('Plat', 2),
+('Plat', 3),
+('Déssert', 4);
+
 -- --------------------------------------------------------
 
 --
@@ -48,6 +58,16 @@ CREATE TABLE IF NOT EXISTS `Avoir_Caracteristiques` (
   KEY `nom_caracteristique` (`nom_caracteristique`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Contenu de la table `Avoir_Caracteristiques`
+--
+
+INSERT INTO `Avoir_Caracteristiques` (`unite`, `valeur`, `nom_ingredient`, `nom_caracteristique`) VALUES
+('bzh', 35, 'Beurre salé', 'Dangereux pour les allergiques à la Bretagne'),
+('kg', 1, 'Chocolat', 'C''est fort en chocolat !'),
+('g', 200, 'Fromage', 'Gras'),
+('kg', 1, 'Pâtes', 'Féculent');
+
 -- --------------------------------------------------------
 
 --
@@ -59,6 +79,16 @@ CREATE TABLE IF NOT EXISTS `Caracteristiques_nutritionnelles` (
   PRIMARY KEY (`nom_caracteristique`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Contenu de la table `Caracteristiques_nutritionnelles`
+--
+
+INSERT INTO `Caracteristiques_nutritionnelles` (`nom_caracteristique`) VALUES
+('C''est fort en chocolat !'),
+('Dangereux pour les allergiques à la Bretagne'),
+('Féculent'),
+('Gras');
+
 -- --------------------------------------------------------
 
 --
@@ -69,6 +99,16 @@ CREATE TABLE IF NOT EXISTS `Categories` (
   `nom_categorie` varchar(255) NOT NULL,
   PRIMARY KEY (`nom_categorie`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Contenu de la table `Categories`
+--
+
+INSERT INTO `Categories` (`nom_categorie`) VALUES
+('Apéritif'),
+('Déssert'),
+('Entrée'),
+('Plat');
 
 -- --------------------------------------------------------
 
@@ -85,6 +125,16 @@ CREATE TABLE IF NOT EXISTS `Commenter` (
   KEY `id_recette` (`id_recette`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Contenu de la table `Commenter`
+--
+
+INSERT INTO `Commenter` (`date`, `texte`, `id_internaute`, `id_recette`) VALUES
+('2016-11-24', 'Moi j''aime bien', 1, 2),
+('2016-11-24', 'Pas mal', 2, 3),
+('2016-11-24', 'Merci pour la recette !', 3, 4),
+('2016-11-24', 'Bloup', 4, 3);
+
 -- --------------------------------------------------------
 
 --
@@ -100,6 +150,16 @@ CREATE TABLE IF NOT EXISTS `Contenir_ingredients` (
   KEY `nom_ingrédient` (`nom_ingrédient`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Contenu de la table `Contenir_ingredients`
+--
+
+INSERT INTO `Contenir_ingredients` (`unité`, `valeur`, `id_recette`, `nom_ingrédient`) VALUES
+('g', 500, 1, 'Chocolat'),
+('g', 200, 2, 'Fromage'),
+('kg', 1, 3, 'Pâtes'),
+('g', 200, 4, 'Beurre salé');
+
 -- --------------------------------------------------------
 
 --
@@ -112,6 +172,17 @@ CREATE TABLE IF NOT EXISTS `Contenir_recette` (
   PRIMARY KEY (`id_recette`,`id_menu`),
   KEY `id_menu` (`id_menu`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Contenu de la table `Contenir_recette`
+--
+
+INSERT INTO `Contenir_recette` (`id_recette`, `id_menu`) VALUES
+(1, 1),
+(3, 2),
+(2, 3),
+(4, 3),
+(4, 4);
 
 -- --------------------------------------------------------
 
@@ -127,7 +198,17 @@ CREATE TABLE IF NOT EXISTS `Descriptions` (
   `id_recette` int(11) NOT NULL,
   PRIMARY KEY (`id_description`),
   KEY `id_recette` (`id_recette`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+
+--
+-- Contenu de la table `Descriptions`
+--
+
+INSERT INTO `Descriptions` (`id_description`, `date_debut`, `date_fin`, `texte`, `id_recette`) VALUES
+(1, '2016-11-24', '0000-00-00', 'C''est fort en chocolat !', 1),
+(2, '2016-11-24', '0000-00-00', 'Miam!', 2),
+(3, '2016-11-24', '0000-00-00', 'Ne pas oublier l''eau !', 3),
+(4, '2016-11-24', '0000-00-00', 'Huummmmm :D', 4);
 
 -- --------------------------------------------------------
 
@@ -140,6 +221,16 @@ CREATE TABLE IF NOT EXISTS `Ingredients` (
   PRIMARY KEY (`nom_ingredient`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Contenu de la table `Ingredients`
+--
+
+INSERT INTO `Ingredients` (`nom_ingredient`) VALUES
+('Beurre salé'),
+('Chocolat'),
+('Fromage'),
+('Pâtes');
+
 -- --------------------------------------------------------
 
 --
@@ -150,7 +241,17 @@ CREATE TABLE IF NOT EXISTS `Internaute` (
   `id_internaute` int(11) NOT NULL AUTO_INCREMENT,
   `pseudo` varchar(255) NOT NULL,
   PRIMARY KEY (`id_internaute`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+
+--
+-- Contenu de la table `Internaute`
+--
+
+INSERT INTO `Internaute` (`id_internaute`, `pseudo`) VALUES
+(1, 'azertyuiop'),
+(2, 'maegrondin'),
+(3, 'sbrouard'),
+(4, 'phenry003');
 
 -- --------------------------------------------------------
 
@@ -164,7 +265,17 @@ CREATE TABLE IF NOT EXISTS `Menu` (
   `id_internaute` int(11) DEFAULT NULL,
   PRIMARY KEY (`id_menu`),
   KEY `id_internaute` (`id_internaute`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+
+--
+-- Contenu de la table `Menu`
+--
+
+INSERT INTO `Menu` (`id_menu`, `nom_menu`, `id_internaute`) VALUES
+(1, 'Menu cinq étoiles', 1),
+(2, 'Menu de Noël', 2),
+(3, 'Menu du RU', 3),
+(4, 'J''ai pas d''idée de nom de menu', 4);
 
 -- --------------------------------------------------------
 
@@ -179,6 +290,16 @@ CREATE TABLE IF NOT EXISTS `Noter` (
   PRIMARY KEY (`id_internaute`,`id_recette`),
   KEY `id_recette` (`id_recette`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Contenu de la table `Noter`
+--
+
+INSERT INTO `Noter` (`valeur`, `id_internaute`, `id_recette`) VALUES
+('1', 1, 1),
+('2', 2, 2),
+('3', 3, 3),
+('3', 4, 4);
 
 -- --------------------------------------------------------
 
@@ -196,7 +317,17 @@ CREATE TABLE IF NOT EXISTS `Recettes_de_cuisine` (
   `id_internaute` int(11) DEFAULT NULL,
   PRIMARY KEY (`id_recette`),
   KEY `id_internaute` (`id_internaute`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+
+--
+-- Contenu de la table `Recettes_de_cuisine`
+--
+
+INSERT INTO `Recettes_de_cuisine` (`id_recette`, `nom_recette`, `date_ajout`, `nombre_personnes`, `temps_preparation`, `temps_cuisson`, `id_internaute`) VALUES
+(1, 'chocapic', '2016-11-24', 1, '08:44:48', '00:00:00', 1),
+(2, 'raclette', '2016-11-24', 10, '00:10:00', '00:30:00', 2),
+(3, 'Pattes à l''eau', '2016-11-24', 2, '00:10:00', '00:10:00', 3),
+(4, 'Caramel au beurre salé', '2016-11-24', 2000, '01:00:00', '00:00:00', 4);
 
 --
 -- Contraintes pour les tables exportées
--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 
+try
 {
 	$bdd = new PDO('mysql:host=localhost;dbname=maegrondin;charset=utf8', 'maegrondin', '76zipive');
 }
