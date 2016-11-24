a5bf428b7ac5c064cc3afa16517b0667bfb45a5b
Simon Brouard <sbrouard@users.noreply.github.com>
2016-11-24 15:57:25 +0000

    identification avec mdp et session

--- identification.php
+++ identification.php
@@ -1,4 +1,6 @@
-<?php include("ouvrir_base.php"); ?>
+<?php 
+
+include("ouvrir_base.php"); ?>
 
 <!DOCTYPE html>
 
@@ -14,26 +16,40 @@
 
 <form method="post" action="identification.php">
 <label for="pseudo">Enter votre pseudo: </label><input type="text" name="pseudo" id="pseudo" placeholder="pseudo" /> <br />
+<label for="password">Entrer votre mot de passe: </label><input type="password" name="password" id="password" placeholder="password" /> <br />
 <input type="submit" value="M'identifier" />
 </form>
 
 
 <?php 
-if (isset($_POST['pseudo'])){ // si on recoit un formulaire complété
-
-	// Vérification de l'unicité du pseudo 
-	$test = $bdd->query('SELECT EXISTS (SELECT id_internaute FROM Internaute WHERE pseudo = "' . $_POST['pseudo'] . '") AS pseudo_exists');
-	$exists = $test->fetch();
-
-	// Si le pseudo existe
-	if ($exists['pseudo_exists']){ 
-		header('Location: index.php');
- 		  exit();
+if (!isset($_POST['pseudo']) || empty($_POST['pseudo'])){ 
+	echo "Veuillez entrer un pseudo ";
+	if (!isset($_POST['password']) || empty($_POST['password'])){ 
+		echo "et un mot de passe";
 	}
+	echo "<br>";
+}
+else{
+	
+	if (!isset($_POST['password']) || empty($_POST['password'])){ 
+		echo "Veuillez entrer un mot de passe<br>";
+	}else{
+	
+		// Vérification de l'existance du pseudo avec correspo,dance du mot de passe
+		$test = $bdd->query('SELECT EXISTS (SELECT id_internaute FROM Internaute WHERE pseudo = "' . $_POST['pseudo'] . '" AND mot_de_passe = "' . $_POST['password'] .'") AS pseudo_exists');
+		$exists = $test->fetch();
 
-	// Si le pseudo n'existe pas
-	else{
-		echo "Pseudo incorrect";
+		// Si le pseudo existe
+		if ($exists['pseudo_exists']){ 
+			$_SESSION['pseudo'] = $_POST['pseudo'];
+			header('Location: index.php');
+			  exit();
+		}
+
+		// Si le pseudo n'existe pas
+		else{
+			echo "Pseudo ou mot de passe incorrect";
+		}
 	}
 }
 ?>
--- ouvrir_base.php
+++ ouvrir_base.php
@@ -1,4 +1,9 @@
 <?php
+
+session_start();
+
+if (isset($_SESSION['pseudo'])) echo $_SESSION['pseudo'];
+
 try
 {
 	$bdd = new PDO('mysql:host=localhost;dbname=maegrondin;charset=utf8', 'maegrondin', '76zipive');
