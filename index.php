e7b3912c53008477eb5fb8de240fa2e02ec74ed3
maegrondin <maegrondin@5a245e59-2e90-4572-920a-9c77d3e03e0c>
2016-11-24 15:16:54 +0000

    modif menu et identification

--- index.php
+++ index.php
@@ -8,10 +8,7 @@
 
 <body>
 
-<p> <a href="liste_recettes.php">Liste des recettes</a> </p>
-<p> <a href="liste_menus.php">Liste des menus</a> </p>
-<p> <a href="inscription.php">Inscription</a> </p>
-<p> <a href="identification.php">Identification</a> </p>
+<?php include("table_matieres.php"); ?>
 
 </body>
 
