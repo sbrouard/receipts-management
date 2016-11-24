c0e84c92401cd7bebd2e62e0d2a218f5beaccfc2
maegrondin <maegrondin@5a245e59-2e90-4572-920a-9c77d3e03e0c>
2016-11-24 11:22:06 +0000

    problème inscription: n'ajoute pas dans la base

--- inscription.php
+++ inscription.php
@@ -12,7 +12,7 @@
 <h2>Inscription</h2>
 
 // Formulaire d'inscription
-<form method="post" action=".">
+<form method="post" action="inscription.php">
 <label for="pseudo">Enter votre pseudo: </label><input type="text" name="pseudo" id="pseudo" placeholder="pseudo" /> <br />
 <input type="submit" value="M'inscrire" />
 </form>
@@ -28,12 +28,12 @@ if (isset($_POST['pseudo'])){ // si on recoit un formulaire complété
 
 	// Si le pseudo n'est pas disponible
 	if ($exists['pseudo_exists']){ 
-		echo "Ce pseudo n'est pas disponible.<br>";
+		echo "<br>Ce pseudo n'est pas disponible.<br>";
 	}
 
 	// Si le pseudo est disponible
 	else{
-		$bdd->exec('INSERT INTO Internaute(pseudo) VALUES($_POST['pseudo']));
+		$bdd->exec('INSERT INTO Internaute(pseudo) VALUES($_POST[\'pseudo\'])');
 		echo "Inscription validée";
 	}
 }
