f61f82585f282f701944c916201e4aafa6883779
Simon Brouard <sbrouard@users.noreply.github.com>
2016-11-24 07:30:22 +0000

    connexion a la base

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+{
+	$bdd = new PDO('mysql:host=localhost;dbname=maegrondin;charset=utf8', 'maegrondin', '76zipive');
+}
+catch (Exception $e)
+{
+        die('Erreur : ' . $e->getMessage());
+}
+
+	?>
+
+<!DOCTYPE html>
+
+<html>
+
+  <head>
+    <meta charset="utf-8"/>
+    <link rel="stylesheet" href="style.css" />
+    <title>Recettes de cuisine</title>
+  </head>
+
+</html>
