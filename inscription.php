236401d40716d90843e0db9d569a64be46f65469
maegrondin <maegrondin@5a245e59-2e90-4572-920a-9c77d3e03e0c>
2016-11-24 10:31:29 +0000

    inscription

--- inscription.php
+++ inscription.php
@@ -11,22 +11,30 @@
 
 <h2>Inscription</h2>
 
-
+// Formulaire d'inscription
 <form method="post" action=".">
 <label for="pseudo">Enter votre pseudo: </label><input type="text" name="pseudo" id="pseudo" placeholder="pseudo" /> <br />
 <input type="submit" value="M'inscrire" />
 </form>
-<?php 
+
+
 // traitement du formulaire d'inscription
+<?php 
 if (isset($_POST['pseudo'])){ // si on recoit un formulaire complété
+
 	// Vérification de l'unicité du pseudo 
 	$test = $bdd->query('SELECT EXISTS (SELECT id_internaute FROM Internaute WHERE pseudo = "' . $_POST['pseudo'] . '") AS pseudo_exists');
 	$exists = $test->fetch();
+
+	// Si le pseudo n'est pas disponible
 	if ($exists['pseudo_exists']){ 
 		echo "Ce pseudo n'est pas disponible.<br>";
 	}
+
+	// Si le pseudo est disponible
 	else{
-		// faire la requete INSERT INTO + affiché inscription validée
+		$bdd->exec('INSERT INTO Internaute(pseudo) VALUES($_POST['pseudo']));
+		echo "Inscription validée";
 	}
 }
 ?>
