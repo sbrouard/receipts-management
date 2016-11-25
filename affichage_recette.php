dad949c4976651c4ca0bb29534b0d042a1967707
maegrondin <maegrondin@5a245e59-2e90-4572-920a-9c77d3e03e0c>
2016-11-25 16:09:21 +0000

    avancée de l'affichage des recettes

--- affichage_recette.php
+++ affichage_recette.php
@@ -10,23 +10,34 @@
 	
 <?php	
 // On récupère le contenu de la recette dans la table Recettes_de cuisine
-$recettes = $bdd->query('SELECT id_recette, nom_recette, 
+$recettes = $bdd->query('SELECT R.id_recette, nom_recette, 
 						DATE_FORMAT(date_ajout, \'%d/%m/%Y\') AS date_ajout_fr, 
-						nombre_personnes, temps_preparation, temps_cuisson, pseudo 
-						FROM Recettes_de_cuisine, Internaute 
-						WHERE Recettes_de_cuisine.id_internaute = Internaute.id_internaute AND
-						id_recette =' . $_GET['id_recette']);
+						nombre_personnes, 
+						DATE_FORMAT(temps_preparation, \'%Hh %imin %ss\') AS temps_prepa, 
+						DATE_FORMAT(temps_cuisson, \'%Hh %imin %ss\') AS temps_cuiss, 
+						pseudo
+						FROM Recettes_de_cuisine AS R INNER JOIN Internaute AS I ON R.id_internaute = I.id_internaute
+						WHERE R.id_recette =' . $_GET['id_recette']);
 						
 // On récupère les informations de la recette						
 $rec = $recettes->fetch();
-echo $rec['pseudo'];
+
+// On récupère les ingrédients et leurs unités
+$ingredients = $bdd->query('SELECT *
+							FROM Contenir_ingredients 
+							WHERE id_recette ='. $_GET['id_recette']);
 
 
 // Affichage de la recette
 echo '<h3>'. $rec['nom_recette'].' (Ajoutée le ' . $rec['date_ajout_fr'] . ' par '. $rec['pseudo'] . ')</h3><br>';
 echo 'Nombre de personnes : '. $rec['nombre_personnes']. '<br>';
-echo 'Temps de préparation : '. $rec['temps_preparation']. '<br>';
-echo 'Temps de cuisson : '. $rec['temps_cuisson']. '<br>';
+echo 'Temps de préparation : '. $rec['temps_prepa']. '<br>';
+echo 'Temps de cuisson : '. $rec['temps_cuiss']. '<br>';
+echo 'Ingrédients nécessaires : ';
+while($ing = $ingredients->fetch()){
+	echo $ing['valeur'].' '.$ing['unite']. ' de ' .$ing['nom_ingrédient'] . ', ';
+}
+echo '<br>';
 ?>
 
 
--- ouvrir_base.php
+++ ouvrir_base.php
@@ -6,7 +6,7 @@ if (isset($_SESSION['pseudo'])) echo $_SESSION['pseudo'];
 
 try
 {
-	$bdd = new PDO('mysql:host=localhost;dbname=maegrondin;charset=utf8', 'maegrondin', '76zipive');
+	$bdd = new PDO('mysql:host=localhost;dbname=maegrondin;charset=utf8', 'maegrondin', '76zipive', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 }
 catch (Exception $e)
 {
