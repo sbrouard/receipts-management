15fd731f96cfd6ca3ed82e04a01578888ee723a3
maegrondin <maegrondin@5a245e59-2e90-4572-920a-9c77d3e03e0c>
2016-11-26 11:51:12 +0000

    affichage recettes, gestion caracteristiques ingrédients : probleme js

--- affichage_recette.php
+++ affichage_recette.php
@@ -13,8 +13,8 @@
 $recettes = $bdd->query('SELECT R.id_recette, nom_recette, 
 						DATE_FORMAT(date_ajout, \'%d/%m/%Y\') AS date_ajout_fr, 
 						nombre_personnes, 
-						DATE_FORMAT(temps_preparation, \'%Hh %imin %ss\') AS temps_prepa, 
-						DATE_FORMAT(temps_cuisson, \'%Hh %imin %ss\') AS temps_cuiss, 
+						DATE_FORMAT(temps_preparation, \'%Hh %imin\') AS temps_prepa, 
+						DATE_FORMAT(temps_cuisson, \'%Hh %imin\') AS temps_cuiss, 
 						pseudo
 						FROM Recettes_de_cuisine AS R INNER JOIN Internaute AS I ON R.id_internaute = I.id_internaute
 						WHERE R.id_recette =' . $_GET['id_recette']);
@@ -29,15 +29,38 @@ $ingredients = $bdd->query('SELECT *
 
 
 // Affichage de la recette
-echo '<h3>'. $rec['nom_recette'].' (Ajoutée le ' . $rec['date_ajout_fr'] . ' par '. $rec['pseudo'] . ')</h3><br>';
+echo '<h3 id="truc">'. $rec['nom_recette'].' (Ajoutée le ' . $rec['date_ajout_fr'] . ' par '. $rec['pseudo'] . ')</h3><br>';
 echo 'Nombre de personnes : '. $rec['nombre_personnes']. '<br>';
 echo 'Temps de préparation : '. $rec['temps_prepa']. '<br>';
 echo 'Temps de cuisson : '. $rec['temps_cuiss']. '<br>';
 echo 'Ingrédients nécessaires : ';
-while($ing = $ingredients->fetch()){
-	echo $ing['valeur'].' '.$ing['unite']. ' de ' .$ing['nom_ingrédient'] . ', ';
+while($ingr = $ingredients->fetch()){
+	
+	// On affiche l'ingrédient
+	echo '<span id="'. $ingr['nom_ingrédient']. '">' .$ingr['valeur'].' '.$ingr['unite']. ' de ' .$ingr['nom_ingrédient'] . ',</span> ';
+	
+	// On récupère les caractéristiques nutritionnelles
+	$caracteristiques = $bdd->query('SELECT * FROM Avoir_Caracteristiques WHERE nom_ingredient ="'. $ingr['nom_ingrédient'] .'"');	
+	echo '<div class="caracteristique" id="'.$ingr['nom_ingrédient'].'_hover">';
+	while($carac = $caracteristiques->fetch()){
+		echo $carac['nom_caracteristique'] . ' : ' . $carac['valeur'] . ' ' . $carac['unite']. '<br>';
+	}
+	echo '</div>';
+?>
+	
+	<!--Gestion du hover pour chaque ingrédient -->
+	<script>
+		var element = document.querySelectorAll('#truc'); //<?php echo $ingr['nom_ingrédient']; ?>
+		alert(element);
+		/*element.addEventListener('mouseover', function(e){
+			e.target.style.top = e.pageX;
+			e.target.style.left = e.pageY;*/
+		//});
+	</script>
+<?php
+
+	echo '<br>';
 }
-echo '<br>';
 ?>
 
 
