1e7a7cd969963cc59d8303662a82e0b545f2903e
maegrondin <maegrondin@5a245e59-2e90-4572-920a-9c77d3e03e0c>
2016-11-29 12:53:44 +0000

    recettes_de_cuisine: fini, index et head modif

--- index.php
+++ index.php
@@ -10,6 +10,13 @@
 
 <?php include("table_matieres.php"); ?>
 
+<p> 
+	<h3>Bienvenue sur Captain Cook !</h3><br> 
+	Sur le menu de gauche, vous pouvez accéder aux meilleures recettes proposées par nos internautes, ainsi qu'aux meilleures sélections de menus divers et variés. <br>
+	Pour participer à la vie du site (rajouter ou commenter des recettes ou menus), merci de bien vouloir vous inscrire ! <br>
+	A bientôt !
+</p>
+
 </body>
 
 </html>
