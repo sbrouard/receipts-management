ea59f20dd410db6603d1cc3a82fab69b27b6ad44
maegrondin <maegrondin@5a245e59-2e90-4572-920a-9c77d3e03e0c>
2016-12-01 09:40:45 +0000

    debut requettes de consultation

--- liste_menus.php
+++ liste_menus.php
@@ -12,14 +12,19 @@
 
 
 <form method="post" action="#">
-<label for="date">Sélectionner uniquement les menus ne comportant que des recettes ajoutées après le: </label><input type="date" name="date" id="date" placeholder="jj/ll/aaaa" maxlength="10" style="width:80;"/>
+<label for="date">Sélectionner uniquement les menus ne comportant que des recettes ajoutées après le: </label><input type="date" name="date" id="date" placeholder="jj/mm/aaaa" maxlength="10" style="width:80;"/>
+<input type="submit" value="sélectionner" />
+</form>
+
+
+<form method="post" action="#">
+Sélectionner uniquement les menus ne comportant que des recettes avec des ingrédients peu caloriques: kcal&lt;
+<input type="number" id="kcal_max" min="0" required style="width:70px;">kcal
 <input type="submit" value="sélectionner" />
 </form>
 
 
 <?php 
-
-
 
 if(isset($_POST['date']) && !empty($_POST['date'])){
 	if(!preg_match("#^[0-3]?[0-9]/(0|1)?[0-9]/[0-2][0-9][0-9][0-9]$#",$_POST['date'])){
@@ -43,6 +48,15 @@ if(isset($_POST['date']) && !empty($_POST['date'])){
 		}
 	}
 }
+
+else if(isset($_POST['kcal_max']) && !empty($_POST['kcal_max'])){
+	$ingredients_peu_cal = $bdd->query('SELECT nom_menu FROM Menu 
+										WHERE id_menu NOT IN 
+										(SELECT Menu.nom_menu FROM Menu, Contenir_recette, Contenir_ingredients, Avoir_Caracteristiques A
+										WHERE (A.nom_caracteristique="Calories" AND valeur<'.$_POST['kcal_max'].') 
+										OR (A.nom_ingredient NOT IN (SELECT nom_ingredient FROM Avoir_Caracteristiques WHERE nom_caracteristique="Calories")))');
+}
+
 else{
 	$menus = $bdd->query('SELECT id_menu,nom_menu FROM Menu');
 	while ($men = $menus->fetch()){
