240608620eb2311a300f34d8251199cf8d529402
Simon Brouard <sbrouard@users.noreply.github.com>
2016-12-06 19:47:44 +0000

    putain ça marche, je suis trop content alors je commit avant de faire des conneries ... Mais cest loin detre fini

--- liste_recettes.php
+++ liste_recettes.php
@@ -37,23 +37,133 @@ while($cat = $categories->fetch()){
 <br><br><br>
 <?php 
 
+//Définition des variables necessaires pour stocker
+//puis regroouper les requettes sucré-salé, top et communes
+
+$sucre_sale ='';
+$top = '';
+$commune = '';
+$req1 = '';$req2 = '';$req3 = '';
+$nb_req = 0;
+$sous_req1 = '';
+
+
+//Petites requettes sucré-salé, top et communes INDEPENDANTES
+
 if(isset($_GET['sucre_sale']) && $_GET['sucre_sale'] == true){
+	$sucre_sale = 'SELECT R.id_recette,nom_recette FROM Recettes_de_cuisine R, Contenir_ingredients C1, Contenir_ingredients C2 WHERE R.id_recette = C1.id_recette = C2.id_recette AND C1.nom_ingrédient = "miel"';
+}
+if(isset($_GET['top']) && $_GET['top'] == true){
+	$top = 'SELECT R.id_recette,nom_recette, COUNT(N.id_recette) AS c FROM Recettes_de_cuisine R, Noter N WHERE R.id_recette = N.id_recette AND N.valeur = 3 GROUP BY N.id_recette HAVING c > 2';
+}
+if(isset($_GET['commune']) && $_GET['commune'] == true){
+	$com_menus = 'SELECT R1.id_recette,nom_recette, COUNT(M1.id_recette) AS nb_menus FROM Recettes_de_cuisine R1, Contenir_recette M1 WHERE R1.id_recette = M1.id_recette GROUP BY M1.id_recette HAVING nb_menus > 2';
+	$com_notes = 'SELECT R2.id_recette,nom_recette, COUNT(N2.id_recette) AS nb_notes FROM Recettes_de_cuisine R2, Noter N2 WHERE R2.id_recette = N2.id_recette GROUP BY N2.id_recette HAVING nb_notes > 9';
+	$com_commentaires = 'SELECT R3.id_recette,nom_recette, COUNT(C3.id_recette) AS nb_com FROM Recettes_de_cuisine R3, Commenter C3 WHERE R3.id_recette = C3.id_recette GROUP BY C3.id_recette HAVING nb_com > 2';
+	$commune = 'SELECT M.id_recette,M.nom_recette 
+				FROM ('. $com_menus .') AS M 
+				INNER JOIN 
+				(SELECT N.id_recette,N.nom_recette FROM ('. $com_notes .') AS N 
+					INNER JOIN 
+					('. $com_commentaires .') AS C
+					ON N.id_recette = C.id_recette) AS L
+				ON M.id_recette = L.id_recette';
+}
 
-	$recettes = $bdd->query('SELECT R.id_recette,nom_recette FROM Recettes_de_cuisine R, Contenir_ingredients C1, Contenir_ingredients C2 WHERE R.id_recette = C1.id_recette = C2.id_recette AND C1.nom_ingrédient = "miel"');
-	while($rec = $recettes->fetch()){
-		echo '<p> <a href="affichage_recette.php?id_recette='.$rec['id_recette'].'">'. $rec['nom_recette'].'</a> </p>';
+
+//Stockage des requettes dans des variables indépendantes de la requete
+	
+if(isset($_GET['sucre_sale']) && $_GET['sucre_sale'] == true){
+	$req1 = $sucre_sale;
+	$nb_req++;
+
+	if(isset($_GET['top']) && $_GET['top'] == true){
+		$req2 = $top;
+		$nb_req++;
+
+		if(isset($_GET['commune']) && $_GET['commune'] == true){
+			$req3 = $commune;
+			$nb_req++;
+		}
+	}	
+
+	else if(isset($_GET['commune']) && $_GET['commune'] == true){
+		$req2 = $commune;
+		$nb_req++;
 	}
-
 }
 else if(isset($_GET['top']) && $_GET['top'] == true){
-	$recettes = $bdd->query('SELECT R.id_recette,nom_recette, COUNT(N.id_recette) AS c FROM Recettes_de_cuisine R, Noter N WHERE R.id_recette = N.id_recette AND N.valeur = 3 GROUP BY N.id_recette HAVING c > 2');
-	while($rec = $recettes->fetch()){
-		echo '<p> <a href="affichage_recette.php?id_recette='.$rec['id_recette'].'">'. $rec['nom_recette'].'</a> </p>';
+	$req1 = $top;
+	$nb_req++;
+
+	if(isset($_GET['commune']) && $_GET['commune'] == true){
+		$req2 = $commune;
+		$nb_req++;
 	}
 }
 else if(isset($_GET['commune']) && $_GET['commune'] == true){
+	$req1 = $commune;
+	$nb_req++;
 }
 
+
+// Regroupement des requêtes sucré-salé, top et communes
+
+if ($nb_req == 1){
+	$sous_req1 = $req1;
+}
+else if ($nb_req == 2){
+	$sous_req1 = 'SELECT * FROM ('. $req1 .' AS REQ1 INNER JOIN ('. $req2 .') AS REQ2 ON REQ1.id_recette = REQ2.id_recette';
+}
+else if ($nb_req == 3){
+
+}
+  
+
+
+// test
+if ($nb_req > 0){
+	$recettes = $bdd->query($sous_req1);
+	while($rec = $recettes->fetch()){
+			echo '<p> <a href="affichage_recette.php?id_recette='.$rec['id_recette'].'">'. $rec['nom_recette'].'</a> </p>';
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*requette sur :
+-categorie
+-nb_personnes
+-date
+*/
+
+
+
+/*
 else{
 	$from = 'Recettes_de_cuisine R ';
 	$where = '';
@@ -108,7 +218,7 @@ else{
 		echo '<p> <a href="affichage_recette.php?id_recette='.$rec['id_recette'].'">'. $rec['nom_recette'].'</a> </p>';
 	}
 }
-
+*/
 
 ?>
 
