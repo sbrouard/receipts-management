9c552546bf44944314654967c79ec142b120115a
sbrouard <sbrouard@5a245e59-2e90-4572-920a-9c77d3e03e0c>
2016-12-06 20:37:05 +0000

    selection des recettes terminee

--- liste_recettes.php
+++ liste_recettes.php
@@ -26,7 +26,11 @@ while($cat = $categories->fetch()){
 <span title="laisser vide pour ignorer">
 <label for="nb_personnes">pour: </label><input type="number" name="nb_personnes" id="nb_personnes" min=0 style="width:50px;" /><label for="nb_personnes">personnes </label>
 <label for="date">ajoutées après le: </label><input type="date" name="date" id="date" placeholder="jj/mm/aaaa" maxlength="10" style="width:80;"/>
-</span>
+</span><br />
+<label for="sucre_sale">Sélectionner seulement les recettes sucré-salé</label><input type="checkbox" name="sucre_sale" id="sucre_sale" /> <br />
+<label for="top">Sélectionner seulement les recettes les plus populaires</label><input type="checkbox" name="top" id="top" /> <br />
+<label for="commune">Sélectionner seulement les recettes les plus utilisées</label><input type="checkbox" name="commune" id="commune" /> <br />
+
 <input type="submit" value="Sélectionner" />
 </form>
 
@@ -50,13 +54,14 @@ $sous_req1 = '';
 
 //Petites requettes sucré-salé, top et communes INDEPENDANTES
 
-if(isset($_GET['sucre_sale']) && $_GET['sucre_sale'] == true){
+//if(isset($_GET['sucre_sale']) && $_GET['sucre_sale'] == true){
+if(isset($_POST['sucre_sale']) && $_POST['sucre_sale'] == true){
 	$sucre_sale = 'SELECT R.id_recette,nom_recette FROM Recettes_de_cuisine R, Contenir_ingredients C1, Contenir_ingredients C2 WHERE R.id_recette = C1.id_recette = C2.id_recette AND C1.nom_ingrédient = "miel"';
 }
-if(isset($_GET['top']) && $_GET['top'] == true){
+if(isset($_POST['top']) && $_POST['top'] == true){
 	$top = 'SELECT R.id_recette,nom_recette, COUNT(N.id_recette) AS c FROM Recettes_de_cuisine R, Noter N WHERE R.id_recette = N.id_recette AND N.valeur = 3 GROUP BY N.id_recette HAVING c > 2';
 }
-if(isset($_GET['commune']) && $_GET['commune'] == true){
+if(isset($_POST['commune']) && $_POST['commune'] == true){
 	$com_menus = 'SELECT R1.id_recette,nom_recette, COUNT(M1.id_recette) AS nb_menus FROM Recettes_de_cuisine R1, Contenir_recette M1 WHERE R1.id_recette = M1.id_recette GROUP BY M1.id_recette HAVING nb_menus > 2';
 	$com_notes = 'SELECT R2.id_recette,nom_recette, COUNT(N2.id_recette) AS nb_notes FROM Recettes_de_cuisine R2, Noter N2 WHERE R2.id_recette = N2.id_recette GROUP BY N2.id_recette HAVING nb_notes > 9';
 	$com_commentaires = 'SELECT R3.id_recette,nom_recette, COUNT(C3.id_recette) AS nb_com FROM Recettes_de_cuisine R3, Commenter C3 WHERE R3.id_recette = C3.id_recette GROUP BY C3.id_recette HAVING nb_com > 2';
@@ -73,35 +78,35 @@ if(isset($_GET['commune']) && $_GET['commune'] == true){
 
 //Stockage des requettes dans des variables indépendantes de la requete
 	
-if(isset($_GET['sucre_sale']) && $_GET['sucre_sale'] == true){
+if(isset($_POST['sucre_sale']) && $_POST['sucre_sale'] == true){
 	$req1 = $sucre_sale;
 	$nb_req++;
 
-	if(isset($_GET['top']) && $_GET['top'] == true){
+	if(isset($_POST['top']) && $_POST['top'] == true){
 		$req2 = $top;
 		$nb_req++;
 
-		if(isset($_GET['commune']) && $_GET['commune'] == true){
+		if(isset($_POST['commune']) && $_POST['commune'] == true){
 			$req3 = $commune;
 			$nb_req++;
 		}
 	}	
 
-	else if(isset($_GET['commune']) && $_GET['commune'] == true){
+	else if(isset($_POST['commune']) && $_POST['commune'] == true){
 		$req2 = $commune;
 		$nb_req++;
 	}
 }
-else if(isset($_GET['top']) && $_GET['top'] == true){
+else if(isset($_POST['top']) && $_POST['top'] == true){
 	$req1 = $top;
 	$nb_req++;
 
-	if(isset($_GET['commune']) && $_GET['commune'] == true){
+	if(isset($_POST['commune']) && $_POST['commune'] == true){
 		$req2 = $commune;
 		$nb_req++;
 	}
 }
-else if(isset($_GET['commune']) && $_GET['commune'] == true){
+else if(isset($_POST['commune']) && $_POST['commune'] == true){
 	$req1 = $commune;
 	$nb_req++;
 }
@@ -113,44 +118,22 @@ if ($nb_req == 1){
 	$sous_req1 = $req1;
 }
 else if ($nb_req == 2){
-	$sous_req1 = 'SELECT * FROM ('. $req1 .' AS REQ1 INNER JOIN ('. $req2 .') AS REQ2 ON REQ1.id_recette = REQ2.id_recette';
+	$sous_req1 = 'SELECT * FROM ('. $req1 .') AS REQ1 INNER JOIN ('. $req2 .') AS REQ2 ON REQ1.id_recette = REQ2.id_recette';
 }
-else if ($nb_req == 3){
+else if ($nb_req == 3){//reste à faire 
+		$sous_req1 = 'SELECT * FROM ('. $req1 .') AS REQ1 
+		INNER JOIN (SELECT REQ2.* FROM ('. $req2 .') AS REQ2 
+			INNER JOIN ('. $req3 .') AS REQ3 
+			ON REQ2.id_recette = REQ3.id_recette) AS REQ4
+		ON REQ1.id_recette = REQ4.id_recette';
 
 }
   
 
-
-// test
-if ($nb_req > 0){
-	$recettes = $bdd->query($sous_req1);
-	while($rec = $recettes->fetch()){
-			echo '<p> <a href="affichage_recette.php?id_recette='.$rec['id_recette'].'">'. $rec['nom_recette'].'</a> </p>';
-	}
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// A ce stade la requete qui sélectionne les recettes
+//sucré-salé et/ou top et/ou communes
+// est stocker dans la variable $sous_req1;
+// il reste à l'assembler avec la requette de sélection sur les catégorie, le nb de personnes et la date qui se trouve ci-dessous
 
 
 
@@ -163,62 +146,78 @@ if ($nb_req > 0){
 
 
 
-/*
-else{
-	$from = 'Recettes_de_cuisine R ';
-	$where = '';
-	$and = false;
-	if(isset($_POST['categorie']) && $_POST['categorie'] != 'toutes'){
-		$from .= ',Appartenir_catégorie C ';
-		$where .= 'WHERE C.nom_catégorie = "' . $_POST['categorie'] . '" AND R.id_recette = C.id_recette ';
-		$and =true;
-	} 
 
-	if(isset($_POST['nb_personnes']) && !empty($_POST['nb_personnes'])){
-		if($and){
-			$where .= 'AND ';
-		}
-		else{
-			$where = 'WHERE ';
-			$and = true;
-		}
-		$where .= 'R.nombre_personnes = '. $_POST['nb_personnes'] .' ';
+$from = 'Recettes_de_cuisine R ';
+$where = '';
+$and = false;
+if(isset($_POST['categorie']) && $_POST['categorie'] != 'toutes'){
+	$from .= ',Appartenir_catégorie C ';
+	$where .= 'WHERE C.nom_catégorie = "' . $_POST['categorie'] . '" AND R.id_recette = C.id_recette ';
+	$and =true;
+} 
+
+if(isset($_POST['nb_personnes']) && !empty($_POST['nb_personnes'])){
+	if($and){
+		$where .= 'AND ';
 	}
+	else{
+		$where = 'WHERE ';
+		$and = true;
+	}
+	$where .= 'R.nombre_personnes = '. $_POST['nb_personnes'] .' ';
+}
 
-	if(isset($_POST['date']) && !empty($_POST['date'])){
-		if(!preg_match("#^[0-3]?[0-9]/(0|1)?[0-9]/[0-2][0-9][0-9][0-9]$#",$_POST['date'])){
-			echo "La date doit être au format \"jj/mm/aaa\"<br>";
+if(isset($_POST['date']) && !empty($_POST['date'])){
+	if(!preg_match("#^[0-3]?[0-9]/(0|1)?[0-9]/[0-2][0-9][0-9][0-9]$#",$_POST['date'])){
+		echo "La date doit être au format \"jj/mm/aaa\"<br>";
+	}
+	else{
+		$date_sql = explode('/',$_POST['date']);
+		if(!checkdate($date_sql[1],$date_sql[0],$date_sql[2])){
+			echo "Cette date n'existe pas";
 		}
 		else{
-			$date_sql = explode('/',$_POST['date']);
-			if(!checkdate($date_sql[1],$date_sql[0],$date_sql[2])){
-				echo "Cette date n'existe pas";
+			if($and){
+				$where .= 'AND ';
 			}
 			else{
-				if($and){
-					$where .= 'AND ';
-				}
-				else{
-					$where = 'WHERE ';
-				}
-				
-				$where .= 'R.date_ajout > "'.$date_sql[2] .'-'. $date_sql[1] .'-'. $date_sql['0'] .'" ';
-				
+				$where = 'WHERE ';
 			}
+			
+			$where .= 'R.date_ajout > "'.$date_sql[2] .'-'. $date_sql[1] .'-'. $date_sql['0'] .'" ';
+			
 		}
 	}
-		
-	$requette = 'SELECT R.id_recette,nom_recette FROM '. $from . $where;
-	$recettes = $bdd->query($requette);
-
-	// On récupère le contenu de la table Recettes_de cuisine
-	//$recettes = $bdd->query('SELECT id_recette, nom_recette FROM Recettes_de_cuisine');
-	//On affiche les lignes une à une:
-	while ($rec = $recettes->fetch()){
-		echo '<p> <a href="affichage_recette.php?id_recette='.$rec['id_recette'].'">'. $rec['nom_recette'].'</a> </p>';
-	}
 }
-*/
+$sous_req2 = 'SELECT R.id_recette,nom_recette FROM '. $from . $where;
+
+
+// la requette de sélection sur les catégorie, le nb de personnes et la date 
+// est à présent stockée dans la variable $sous_req2
+
+
+
+// On réuni les "petites" requêtes en la requête principale:
+$requette = '';
+if ($nb_req > 0){
+$requette = 'SELECT * FROM ('. $sous_req1 .') AS SOUS_REQ1
+						INNER JOIN ('. $sous_req2 .') AS SOUS_REQ2 ON SOUS_REQ1.id_recette = SOUS_REQ2.id_recette';
+}
+else{
+	$requette = $sous_req2;
+}
+
+echo $requette .'<br><br>';
+$recettes = $bdd->query($requette);
+
+
+
+
+// affichage du résultat ligne par ligne sous forme de lien
+while ($rec = $recettes->fetch()){
+	echo '<p> <a href="affichage_recette.php?id_recette='.$rec['id_recette'].'">'. $rec['nom_recette'].'</a> </p>';
+}
+
 
 ?>
 
