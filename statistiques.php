a5af04c9509de1f5156bec7f3a9e4631ab1d1fc4
phenry003 <phenry003@5a245e59-2e90-4572-920a-9c77d3e03e0c>
2016-12-07 16:58:02 +0000

    ARGH-statistique

--- statistiques.php
+++ statistiques.php
@@ -76,6 +76,99 @@ if(isset($_SESSION['pseudo'])){
 }
 	
 ?>
+
+<h3 id="truc">Classement des ingrédients :</h3><br>
+
+	
+<?php	
+
+// On récupère les ingrédients
+/*$ingrédients = $bdd->query('SELECT I.nom_ingredient, (SELECT AVG(N.valeur)
+						FROM Ingredients I, Noter N, Recettes_de_cuisine R, Contenir_ingredients C
+						WHERE C.nom_ingrédient = I.nom_ingredient AND C.id_recette = R.id_recette AND N.id_recette = R.id_recette
+						GROUP BY N.id_recette)*((SELECT valeur FROM Avoir_Caracteristiques WHERE unite = "kcal")/(SELECT SUM(valeur) FROM Avoir_Caracteristiques WHERE unite = "kcal")) 
+						FROM Ingredients I');*/
+
+//OK
+$moyenne_par_ingrédient = $bdd->query('SELECT C.nom_ingrédient, AVG(Mo) AS Av 
+						FROM (SELECT R.id_recette, AVG(N.valeur) AS Mo
+						FROM Noter N, Recettes_de_cuisine R
+						WHERE N.id_recette = R.id_recette
+						GROUP BY N.id_recette) A, Contenir_ingredients C
+						WHERE A.id_recette = C.id_recette
+						GROUP BY C.nom_ingrédient');
+//OK					
+$ratio_calories = $bdd->query('SELECT nom_ingredient,(valeur/ 
+						(SELECT SUM(valeur) 
+						FROM Avoir_Caracteristiques 
+						WHERE unite = "kcal")) AS S 
+						FROM Avoir_Caracteristiques 
+						WHERE unite = "kcal"');
+//OK
+$nb_com_par_rec = $bdd->query('SELECT id_recette, COUNT(texte) AS N
+						FROM Commenter
+						GROUP BY id_recette');
+						
+$ingredients = $bdd->query('SELECT nom_ingrédient, valeur AS Somme
+					FROM Contenir_ingredients');
+								
+$d;
+
+	
+//OK	
+while($c = $nb_com_par_rec->fetch()){
+	//echo '<p class="nom_ingredient" id="coucou "> '.$c['id_recette'] . ' :' .$c['N']. '</p> ';
+	//echo '</span>';
+	if( $c['N'] <= 3){
+	$d[$c['id_recette']] = '1';
+	}
+	elseif( $c['N'] <= 10){
+	$d[$c['id_recette']] = 2;
+	}
+	else{
+	$d[$c['id_recette']] = 3;
+	}
+	echo '<p class="nom_ingredient" id="coucou "> '.$c['id_recette'] . ' :' .$d[$c['id_recette']]. '</p> ';
+	echo '</span>';
+	
+}					
+	
+while($i = $ingredients->fetch()){		
+		$i['Somme'] = 0;
+		$recettes = $bdd->query('SELECT C.id_recette FROM Contenir_ingredients C 
+		WHERE C.nom_ingrédient = "' . $i['nom_ingrédient'] );
+		while($e = $recettes->fetch()){			
+			$i['Somme'] += $d[$e['id_recette']];	
+			echo 'ajout';	
+		}
+	echo '<p class="nom_ingredient" id="coucou "> '.$d['nom_ingrédient'] . ' ::' .$d['Somme']. '</p> ';
+	echo '</span>';
+}
+
+//while($c = $somme_coef_com->fetch()){
+//	echo '<p class="nom_ingredient" id="'. $a['Mo']. '">' .$a['nom_ingrédient']. ' :' .$a['K']. '</p> ';
+//	echo '</span>';
+
+
+while($b = $ratio_calories->fetch()){
+	echo '<p class="nom_ingredient" id="coucou "> '.$b['nom_ingredient'] . ' :' .$b['S']. '</p> ';
+	echo '</span>';
+}
+
+while($a = $moyenne_par_ingrédient->fetch()){
+	echo '<p class="nom_ingredient" id="'. $a['Mo']. '">' .$a['nom_ingrédient']. ' :' .$a['K']. '</p> ';
+	echo '</span>';
+	
+							
+
+// On affiche le tout
+//while($in = $ingrédients->fetch()){
+	// On affiche les recettes triées par note
+	//echo '<p class="nom_ingredient" id="'. $in['nom_ingredient']. '">' .$in['nom_ingredient']. ' : ?? </p> ';
+	//echo '</span>';
+
+}
+?>
 		
 </section>		
 </body>
